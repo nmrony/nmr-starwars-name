8d7ec457ed0d3386d15fefc70433ef3e4f8d23f3
Nur Rony <pro.nmrony@gmail.com>
2016-11-07 01:37:19 +0600

    adds files after code coverage and es2015 support

---
+++ README.md
@@ -0,0 +1,4 @@
+Starwars Names
+--------------
+
+Just a demo for how to do open-source project ideally 
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import starWarsNames from './starwars-names.json';
+import uniqueRandomArray from 'unique-random-array';
+
+const getRandomItem = uniqueRandomArray(starWarsNames)
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem()
+  }
+
+  let randomItems = [];
+  for(var i = 0; i < number; i++) {
+    randomItems.push(getRandomItem());
+  }
+  return randomItems;
+}
+
+module.exports = {
+  all: starWarsNames,
+  random
+};
